056761837e8552635be48af397f28396184f90b8
Timothy <timothy.chongz.luong@gmail.com>
2016-04-28 15:59:26 -0700

    Tabs added

---
+++ iKid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Timothy Luong on 4/28/16.
+//  Copyright © 2016 Timothy Luong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
